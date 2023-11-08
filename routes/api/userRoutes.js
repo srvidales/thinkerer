1281d7577030d8bcce627ab11a38e1d055e92fb4
Sergio Vidales Perez <srvidales@gmail.com>
2023-11-07 16:57:40 -0800

    Initial routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  //   getStudents,
+  //   getSingleStudent,
+  //   createStudent,
+  //   deleteStudent,
+  //   addAssignment,
+  //   removeAssignment,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers);
+
+// /api/users
+// router.route('/').get(getUsers).post(createUser);
+
+// /api/students/:studentId
+// router.route('/:studentId').get(getSingleStudent).delete(deleteStudent);
+
+// /api/students/:studentId/assignments
+// router.route('/:studentId/assignments').post(addAssignment);
+
+// /api/students/:studentId/assignments/:assignmentId
+// router.route('/:studentId/assignments/:assignmentId').delete(removeAssignment);
+
+module.exports = router;
